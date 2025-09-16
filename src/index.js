99f5b427a4e40acfce3802434628e210dea9623a
vicky-voin <alteneria127@gmail.com>
2025-09-16 11:28:19 -0400

    Added the necessary html elements (not styled)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./main.css"
+import "./variables.css"
+
+const projects = [];
+
+const projectTemplate = document.getElementsByTagName("template")[0];
+const projectsContainer = document.querySelector(".projects-content");
+
+for(let i = 0; i < 6; i++)
+{
+    const projectClone = projectTemplate.content.cloneNode(true);
+    projects.push(projectsContainer.appendChild(projectClone));
+}
